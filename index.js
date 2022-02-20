0ff2bded661f5e524d59d88a7a04886cecefb369
Michael Bowen <michaelbowen1092@gmail.com>
2022-02-20 08:50:16 -0500

    commit tree fe39d256b5e23e5b7ee7c4d26da6a71a59840320

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+    return 'This one is on me!'
+  }
+  else if(distance >= 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(distance > 2500){
+    return 'No can do.'
+  }
 }
